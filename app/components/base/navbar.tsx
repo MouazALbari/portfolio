1627cafad8331710346c37d500fea6820f9b424a
Mouaz Albari <moazb8948@gmail.com>
2025-09-28 14:58:00 +0300

    Create navbar.tsx

---
+++ app/components/base/navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { NAV_LINKS } from "@/constants";
+
+export const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001427] backdrop-blur-md z-50 px-10">
+      {/* Navbar Container */}
+      <div className="w-full h-full flex items-center justify-between m-auto px-[10px]">
+        {/* Logo + Name */}
+        <Link href="#about-me" className="flex items-center">
+          <div className="hidden text-2xl font-bold md:flex text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 md:selffont-bold ml-[10px] ">
+       
+           {" I'm Mouaz Albar"}
+          </div>
+        </Link>
+
+        {/* Web Navbar */}
+        <div className="hidden md:flex w-[500px] h-full flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto border-[rgba(112,66,248,0.38)] bg-[rgba(3,0,20,0.37)] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            {NAV_LINKS.map((link) => (
+              <Link
+                key={link.title}
+                href={link.link}
+                className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div></div>
+
+        <button
+          className="md:hidden text-white focus:outline-none text-4xl"
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          ☰
+        </button>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="absolute top-[65px] left-0 w-full bg-[#030014] p-5 flex flex-col items-center text-gray-300 md:hidden">
+          <div className="flex flex-col items-center gap-4">
+            {NAV_LINKS.map((link) => (
+              <Link
+                key={link.title}
+                href={link.link}
+                className="cursor-pointer hover:text-[rgb(112,66,248)] transition text-center"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
