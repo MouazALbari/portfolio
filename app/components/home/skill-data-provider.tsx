55386cad0dbaef2509a8fb957c1e475f60a99648
Mouaz Albari <moazb8948@gmail.com>
2025-09-28 14:58:05 +0300

    Create skill-data-provider.tsx

---
+++ app/components/home/skill-data-provider.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { IconType } from "react-icons";
+
+type SkillDataProviderProps = {
+  Icon: IconType;
+  name: string;
+  index: number;
+  size?: number;
+  color?: string; // لون الأيقونة
+};
+
+export const SkillDataProvider = ({
+  Icon,
+  index,
+  size = 80,
+  color = "#000",
+}: SkillDataProviderProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const iconVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const animationDelay = 0.1;
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      variants={iconVariants}
+      animate={inView ? "visible" : "hidden"}
+      custom={index}
+      transition={{ delay: index * animationDelay }}
+      className="flex flex-col items-center gap-2"
+    >
+      <Icon size={size} className="max-sm:w-10 max-sm:h-10" color={color} />
+    </motion.div>
+  );
+};
